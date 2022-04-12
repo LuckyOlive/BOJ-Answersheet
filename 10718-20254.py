d414f1004aa0a0d2ca5c91a49ee67f8f1ea75f6e
roddy <highend02@naver.com>
2022-04-12 16:44:43 +0900

    This is Bronze IV anwsersheet

---
+++ 10718-20254.py
@@ -0,0 +1,159 @@
+# 10718
+
+print("""강한친구 대한육군
+강한친구 대한육군""")
+
+# 10757
+
+A, B = map(int, input().split())
+print(A+B)
+
+# 10869
+
+A, B = map(int, input().split())
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+
+# 10926
+
+id = str(input())
+print(id+"??!")
+
+# 10998
+
+A, B = map(int, input().split())
+print(A*B)
+
+# 11283
+
+print(ord(input())-44031)
+
+# 11382
+
+A, B, C = map(int, input().split())
+print(A+B+C)
+
+# 11654
+
+print(ord(input()))
+
+# 11942
+
+print("고려대학교")
+
+# 14645
+
+A, B = map(int, input().split())
+for i in range(A) :
+    C, D = map(int, input().split())
+print("비와이")
+
+# 14652
+
+A, B, C = map(int, input().split())
+ansX = int(C/B)
+ansY = C%B
+
+print(ansX,ansY)
+
+# 15439
+
+a = int(input())
+print(a*a-a)
+
+# 15727
+
+a = int(input())
+if a % 5 == 0 :
+    print(a//5)
+else :
+    print(a//5 + 1)
+
+# 15733
+
+print("I'm Sexy")
+
+# 15740
+
+A, B = map(int, input().split())
+print(A+B)
+
+# 15894
+
+a = int(input())
+
+print(a*4)
+
+# 15962
+
+print("파이팅!!")
+
+# 15964
+
+A, B = map(int, input().split())
+C = (A+B) * (A-B)
+print(C)
+
+# 16170
+
+import datetime
+
+date = datetime.datetime.now()
+print(date.year)
+print(date.month)
+print(date.day)
+
+# 16394
+
+a = int(input())
+ans = a - 1946
+print(ans)
+
+# 16430
+
+a, b = map(int, input().split())
+print(b-a, b)
+
+# 17256
+
+ax, ay, az = map(int, input().split())
+cx, cy, cz = map(int, input().split())
+
+bx = cx - az
+by = cy // ay
+bz = cz - ax
+
+print(bx, by, bz)
+
+# 17295
+
+print('Avengers: Endgame')
+
+# 17496
+
+a, b, c, d = map(int, input().split())
+if a % b == 0 :
+    ans = (a // b - 1) * c * d
+    print(ans)
+else:
+    ans = (a // b) * c * d
+    print(ans)
+
+# 18108
+
+a = int(input())
+print(a-543)
+
+# 18301
+
+a, b, c = map(int, input().split())
+
+print((a+1)*(b+1)//(c+1)-1)
+
+# 20254
+
+a, b, c, d = map(int, input().split())
+print(56*a + 24*b + 14*c + 6*d)
