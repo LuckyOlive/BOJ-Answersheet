b79ad9bd47e25621ed1d295bcec92c3330e9ce5d
roddy <highend02@naver.com>
2022-04-19 23:58:46 +0900

    This is answersheet

---
+++ examples.py
@@ -0,0 +1,10 @@
+>>> score = [90, 95, 100] # 대괄호로 리스트 만드는 법
+>>> print(score)
+[90, 95, 100]
+
+>>> grade = [] # 공백 리스트에 추가하는 방법
+>>> grade.append(int(90))
+>>> grade.append(int(95))
+>>> grade.append(int(100))
+>>> print(grade)
+[90, 95, 100]
